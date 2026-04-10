06c4c56731c4ee8562c5940ea646c0d051f73aee
Sansnm09 <162101302+Sansnm09@users.noreply.github.com>
2026-04-10 15:27:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,15 @@
-const API_KEY = "943a0bae34mshe1eab6d31155808p12538djsn6e90e57ffe12"
+const API_KEY="943a0bae34mshe1eab6d31155808p12538djsn6e90e57ffe12"
 
-let queue = []
+let queue=[]
+let currentIndex=0
 
 async function searchSongs(){
 
-const query = document.getElementById("searchInput").value
+const query=document.getElementById("searchInput").value
 
-if(query===""){
-alert("Please enter a song name")
-return
-}
+const url=`https://spotify23.p.rapidapi.com/search/?q=${query}&type=tracks&limit=6`
 
-const url = `https://spotify23.p.rapidapi.com/search/?q=${query}&type=tracks&limit=5`
-
-const options = {
+const options={
 method:'GET',
 headers:{
 'X-RapidAPI-Key':API_KEY,
@@ -21,66 +17,81 @@ headers:{
 }
 }
 
-try{
+const response=await fetch(url,options)
+const data=await response.json()
 
-const response = await fetch(url,options)
-const data = await response.json()
-
-const tracks = data.tracks.items
+const tracks=data.tracks.items
 
 let html=""
 
 tracks.forEach(item=>{
 
-const track = item.data
+const track=item.data
 
-html += `
+html+=`
 <div class="track">
-
-<img src="${track.albumOfTrack.coverArt.sources[0].url}">
 
 <p>${track.name} - ${track.artists.items[0].profile.name}</p>
 
 <button onclick="playSong('${track.id}')">Play</button>
 
-<button onclick="addQueue('${track.id}','${track.name}')">Add to Queue</button>
+<button onclick="addQueue('${track.id}','${track.name}')">
+Add to Queue
+</button>
 
 </div>
 `
 })
 
-document.getElementById("results").innerHTML = html
-
-}
-
-catch(error){
-console.log(error)
-alert("Error fetching songs")
-}
-
+document.getElementById("results").innerHTML=html
 }
 
 function playSong(id){
 
-document.getElementById("player").innerHTML =
-`<iframe src="https://open.spotify.com/embed/track/${id}" width="320" height="80" frameborder="0" allow="encrypted-media"></iframe>`
-
+document.getElementById("player").innerHTML=
+`<iframe src="https://open.spotify.com/embed/track/${id}" width="400" height="100"></iframe>`
 }
 
 function addQueue(id,name){
 
 queue.push({id,name})
-updateQueue()
 
+updateQueue()
 }
 
 function updateQueue(){
 
 let html=""
 
-queue.forEach(song=>{
-html += `<li>${song.name}</li>`
+queue.forEach((song,index)=>{
+html+=`<li>${song.name}</li>`
 })
 
-document.getElementById("queue").innerHTML = html
+document.getElementById("queue").innerHTML=html
+}
+
+function nextSong(){
+
+if(queue.length===0) return
+
+currentIndex++
+
+if(currentIndex>=queue.length){
+currentIndex=0
+}
+
+playSong(queue[currentIndex].id)
+}
+
+function previousSong(){
+
+if(queue.length===0) return
+
+currentIndex--
+
+if(currentIndex<0){
+currentIndex=queue.length-1
+}
+
+playSong(queue[currentIndex].id)
 }
